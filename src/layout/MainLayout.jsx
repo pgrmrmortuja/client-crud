ee3ff5283b8ffc8f9fb58fa1da4ca052b3bf61d8
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-08-11 01:27:44 +0600

    installed and setpu tailwind css

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1 className='text-red-500'>Home</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {
+  RouterProvider,
+} from "react-router";
+import ReactDOM from "react-dom/client";
+import { router } from "./Router/Route";
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
