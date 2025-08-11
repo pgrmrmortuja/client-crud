6762bf0410ac4c2839217d1982ecb5bfab47bfe2
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-08-11 12:54:21 +0600

    remove shadcn ui

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,10 @@
-// import { Button } from '../../src/components/ui/button';
-import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const MainLayout = () => {
     return (
         <div>
             <h1 className='text-red-500'>Home</h1>
-            <Button>Shadcn Ui Button</Button>
+          
         </div>
     );
 };
