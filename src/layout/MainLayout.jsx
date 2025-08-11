bbeadbc6f353030771dd68987bd50fdfba1bed0b
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-08-11 14:48:02 +0600

    successfully installed and use daisyui

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,13 @@
+
 import React from 'react';
+
 
 const MainLayout = () => {
     return (
         <div>
             <h1 className='text-red-500'>Home</h1>
-          
+            <button className="btn btn-soft btn-success">Success</button>
+           
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,12 @@ import ReactDOM from "react-dom/client";
 import { router } from "./Router/Route";
 import './index.css';
 
+
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <RouterProvider router={router} />,
+
+  <RouterProvider router={router} />
+
+
 );
