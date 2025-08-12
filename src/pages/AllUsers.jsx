309fb2dd98c9a1b0f122d721556062af3bc8119b
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-08-12 20:08:55 +0600

    get all users list

--- src/pages/AllUsers.jsx
+++ src/pages/AllUsers.jsx
@@ -1,64 +1,58 @@
-import React, { useContext, useState } from 'react';
-import { AuthContext } from '../providers/AuthProvider';
+import React, { useEffect, useState } from 'react';
+
 
 const AllUsers = () => {
-    const {user, setUser} = useState("");
-    
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/users")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("get user list data", data);
+                setUsers(data);
+            })
+            .catch(err => console.log(err))
+    }, [])
+
 
     return (
         <div className='w-11/12 mx-auto'>
             <h1 className='text-center text-2xl mb-5'>User List</h1>
 
-            {
-                user ?
-                    (
-                        <div className="overflow-x-auto">
-                            <table className="table">
-                                {/* head */}
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Email</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {/* row 1 */}
-                                    <tr>
-                                        
-                                        <td>
-                                            <div className="flex items-center gap-3">
-                                                {/* <div className="avatar">
-                                                    <div className="mask mask-squircle h-12 w-12">
-                                                        <img
-                                                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                                                            alt="Avatar Tailwind CSS Component" />
-                                                    </div>
-                                                </div> */}
-                                                <div>
-                                                    <div className="font-bold">Hart Hagerty</div>
-                                        
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            Zemlak, Daniel and Leannon
-                                           
-                                        </td>
-                                        
-                                        <th>
-                                            <button className="btn btn-ghost btn-xs">details</button>
-                                        </th>
-                                    </tr>
-                                    
-                                </tbody>
-                            </table>
-                        </div>
-                    ) :
-                    (
-                        <p className='text-red-500 text-center'>No User Found</p>
-                    )
-            }
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>SI</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            users.map((user, index) => (
+                                <tr key={user._id}>
+                                    <td>{index + 1}</td>
+
+                                    <td>
+                                                <div className="font-bold">{user.name}</div>
+ 
+                                    </td>
+                                    <td>{user.email}</td>
+
+                                    <th>
+                                        <button className="btn btn-ghost btn-xs">details</button>
+                                    </th>
+                                </tr>
+                            ))
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
