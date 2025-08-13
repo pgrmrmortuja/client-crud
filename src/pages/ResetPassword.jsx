b12745631b6554214939d0bd6daceb51bd0d7bd2
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-08-13 20:59:59 +0600

    successfully reset password

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,35 +1,117 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
+
 const ResetPassword = () => {
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [password, setPassword] = useState("");
+    const [account, setAccount] = useState(null);
+
+    const findAccount = () => {
+        fetch(`http://localhost:5000/find-account?email=${email}`)
+        .then((res) =>res.json())
+        .then((data) => {
+            console.log(data)
+            if(data.name && data.email){
+                setAccount(data);
+                Swal.fire("Account Found, Now Reset Password")
+            } else{
+                Swal.fire(`${data.message}`)
+            }
+        })
+        .catch(err => console.log(err))
+    }
+
+    const resetPassword = (e) => {
+        e.preventDefault();
+
+        if(password !== newPassword){
+            return Swal.fire("Password Should be Matched");
+        }
+
+        fetch("http://localhost:5000/reset-password",{
+            method: "PATCH",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                newPassword: newPassword
+            })
+        })
+        .then((res) =>res.json())
+        .then((data) => {
+            console.log("reset password",data)
+            if(data.result && data.result.modifiedCount > 0){
+                Swal.fire(`${data.message}`)
+                navigate("/login");
+            }
+            else{
+                Swal.fire(`${data.message}`)
+            }
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
-        <div className="min-h-screen flex items-center justify-center bg-base-100 px-4">
-            <form  className="w-full max-w-lg">
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-6 shadow-md">
-                    <legend className="fieldset-legend text-lg font-semibold text-center">
-                        Forgotten Password?
-                    </legend>
+        <div className="max-w-sm mx-auto px-4">
 
-                    {/* Email */}
-                    <label className="label">Email</label>
-                    <input
-                        type="email"
-                        className="input input-bordered w-full"
-                        name='email'
-                        placeholder="Email"
-                    />
+            {/* Find Account */}
+            {
+                !account && (
+                    <div className="space-y-2">
+                        <label className="label mt-2">Email</label>
+                        <input
+                            type="email"
+                            name='email'
+                            className="input input-bordered w-full"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
 
-                    {/* Password */}
-                    <label className="label mt-2">Password</label>
-                    <input
-                        type="password"
-                        name='password'
-                        className="input input-bordered w-full"
-                        placeholder="Password"
-                    />
+                        <button onClick={findAccount} className="btn btn-neutral mt-4 w-full">Find Account</button>
+                    </div>
+                )
+            }
 
-                    {/* Login Button */}
-                    <button className="btn btn-neutral mt-4 w-full">Reset Password</button>
 
-                </fieldset>
-            </form>
+            {/* Reset Password */}
+            {
+                account && (
+                    <form onSubmit={resetPassword} className="space-y-2">
+                        <p>Name: {account.name}</p>
+                        <p>Email: {account.email}</p>
+
+                        <label className="label mt-2">New Password</label>
+                        <input
+                            type="password"
+                            className="input input-bordered w-full"
+                            placeholder="New Password"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label className="label mt-2">Confirm New Password</label>
+                        <input
+                            type="password"
+                            name='password'
+                            className="input input-bordered w-full"
+                            placeholder="Confirm Password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            required
+                        />
+
+                        <button className="btn btn-neutral mt-4 w-full">Reset Password</button>
+                    </form>
+                )
+            }
+
+
         </div>
     );
 };
